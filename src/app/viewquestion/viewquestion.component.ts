eb3cedfb1222324a777675bcd51cadef6ffcf735
s.sriramprasath@gmail.com <s.sriramprasath@gmail.com>
2018-10-28 20:53:06 +0530

    updated

---
+++ src/app/viewquestion/viewquestion.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { MCQService } from '../mcq.service';
+@Component({
+  selector: 'app-viewquestion',
+  templateUrl: './viewquestion.component.html',
+  styleUrls: ['./viewquestion.component.css']
+})
+export class ViewquestionComponent implements OnInit {
+private myarray:any;
+  constructor(private mcqservice :MCQService) { }
+
+
+  ngOnInit() {
+    this.viewall();
+  }
+
+
+  viewall()
+  {
+    this.mcqservice.read().subscribe(
+      data => {
+        console.log(data)
+        this.myarray = data;
+      },
+      error =>
+      {
+         console.log(error);
+      }
+
+    )
+}
+}
